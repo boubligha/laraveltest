c363a96081743a69d6fb731a1d9013c02f5e2dea
boubligha <151193079+boubligha@users.noreply.github.com>
2025-02-06 08:01:42 +0100

    working on  imagration of models

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -5,13 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-    <title>social network |  @yield('title','default-title')</title>
+    <title>social network | {{$title}}</title>
 </head>
 <body>
     @include('partials.nav')
     <main>
-        <div class="m-5">
-            @yield('main')
+        <div class="m-3">
+            {{ $slot }}
         </div>
     </main>
     @include('partials.footer')
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,13 @@
-@extends('layouts.master')
-@section('title')
- Home
-@endsection
-@section('main')
-<h1>welcome to ur home</h1>
+<x-master title="Home">
+        <h1>Welcome to your home</h1>
+        {{-- Passing data using slot --}}
 
-//passing data to a component using props
-<x-users-table :users="$users"/>
-@endsection
-
-
+        <x-alert type="warning">
+            <strong>Salam</strong>
+        </x-alert>
+        <x-alert type="danger">
+            <strong>hi</strong>
+        </x-alert>
+        {{-- Passing data to a component using props --}}
+        <x-users-table :users="$users"/>
+</x-master>
