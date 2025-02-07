f3207eca95bd94ed64ca807e19d2cc9bcc350d77
boubligha <151193079+boubligha@users.noreply.github.com>
2025-02-07 19:03:58 +0100

    working with flash backs

--- resources/views/components/master.blade.php
+++ resources/views/components/master.blade.php
@@ -11,6 +11,11 @@
     @include('partials.nav')
     <main>
         <div class="container">
+            @if (session()->has('success'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('success') }}
+                </div>
+            @endif
             {{ $slot }}
         </div>
     </main>
